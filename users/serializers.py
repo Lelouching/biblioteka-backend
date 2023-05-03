43cdcf759b33c87b62cf82b9a49722232f5e83ac
Nath Medeiros <nathalia_lalvc@hotmail.com>
2023-05-03 11:27:06 -0300

    create user routes

--- users/serializers.py
+++ users/serializers.py
@@ -1,53 +1,8 @@
 from rest_framework import serializers
-from users.models import User
-from rest_framework.validators import UniqueValidator
+from .models import User
 
 
 class UserSerializer(serializers.ModelSerializer):
-    username = serializers.CharField(
-        validators=[
-            UniqueValidator(
-                queryset=User.objects.all(),
-                message="A user with that username already exists.",
-            )
-        ],
-    )
-
-    email = serializers.EmailField(
-        validators=[UniqueValidator(queryset=User.objects.all())],
-    )
-
-    def create(self, validated_data: dict) -> User:
-        password = validated_data.pop("password")
-        user = User(**validated_data)
-        user.set_password(password)
-        user.save()
-        return user
-
-    def update(self, instance: User, validated_data: dict) -> User:
-        for key, value in validated_data.items():
-            if key == "password":
-                instance.set_password(value)
-            else:
-                setattr(instance, key, value)
-
-        instance.save()
-
-        return instance
-
     class Meta:
         model = User
-        fields = [
-            "id",
-            "username",
-            "email",
-            "password",
-            "first_name",
-            "last_name",
-            "student",
-            "blocked",
-            "super_user",
-        ]
-        read_only_fields = ["id"]
-
-        extra_kwargs = {"password": {"write_only": True}}
+        fields = "__all__"
--- users/views.py
+++ users/views.py
@@ -1,8 +1,50 @@
-from rest_framework.generics import CreateAPIView
-from .serializers import UserSerializer
+from rest_framework import generics, permissions
 from .models import User
+from .serializers import UserSerializer
 
 
-class UserView(CreateAPIView):
+class UserListAPIView(generics.ListAPIView):
     serializer_class = UserSerializer
     queryset = User.objects.all()
+    permission_classes = [permissions.IsAuthenticated, permissions.IsAdminUser]
+
+
+class UserDetailAPIView(generics.RetrieveAPIView):
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
+    permission_classes = [permissions.IsAuthenticated, permissions.IsAdminUser]
+
+
+class UserCreateAPIView(generics.CreateAPIView):
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
+
+
+class UserUpdateAPIView(generics.RetrieveUpdateAPIView):
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_object(self):
+        user = super().get_object()
+        if self.request.user.is_staff or user == self.request.user:
+            return user
+        else:
+            raise permissions.PermissionDenied(
+                "You do not have permission to update this user."
+            )
+
+
+class UserDeleteAPIView(generics.DestroyAPIView):
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_object(self):
+        user = super().get_object()
+        if self.request.user.is_staff or user == self.request.user:
+            return user
+        else:
+            raise permissions.PermissionDenied(
+                "You do not have permission to delete this user."
+            )
