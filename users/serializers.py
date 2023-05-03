a3676952cf6b5e76fbbb6eb8fdb1de4f0d1274e3
Nath Medeiros <nathalia_lalvc@hotmail.com>
2023-05-03 13:57:02 -0300

    att user serializer

--- users/serializers.py
+++ users/serializers.py
@@ -3,6 +3,24 @@ from .models import User
 
 
 class UserSerializer(serializers.ModelSerializer):
+    def create(self, validated_data: dict) -> User:
+        return User.objects.create_superuser(**validated_data)
+
+    def update(self, instance: User, validated_data: dict) -> User:
+        for key, value in validated_data.items():
+            if key == "password":
+                instance.set_password(value)
+            else:
+                setattr(instance, key, value)
+
+        instance.save()
+
+        return instance
+
     class Meta:
         model = User
         fields = "__all__"
+
+        read_only_fields = ["id"]
+
+        extra_kwargs = {"password": {"write_only": True}}
